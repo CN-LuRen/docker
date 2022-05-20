071889d3c0f74fe3ff567595744d33cb562c22d7
再见亦是路人 <15252114322@163.com>
2022-05-20 21:27:44 +0800

    add node_exporter

---
+++ shell/node_exporter.sh
@@ -0,0 +1,17 @@
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+tar vxf node_exporter-1.3.1.linux-amd64.tar.gz
+cat > /etc/systemd/system/node_exporter.service << EOF
+[Unit]
+Description=node_exporter
+Documentation=https://prometheus.io/
+After=network.target
+[Service]
+Type=simple
+User=prometheus
+ExecStart=/usr/local/bin/node_exporter
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl start node_exporter
+systemctl enable node_exporter
