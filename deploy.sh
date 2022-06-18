d747f9373dc071b1fb4459d21185abc30ba7a693
再见亦是路人 <15252114322@163.com>
2022-06-19 00:08:02 +0800

    sh update

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,16 @@
 
 #使用说明，用来提示输入参数
 usage() {
-    echo "|-------------------------------------------|"
-    echo "| usage: sh deploy.sh |mount|               |"
-    echo "|-------------------------------------------|"
-    echo "| mount |  mount config files to /docker    |"
-    echo "|-------------------------------------------|"
+    echo "| usage: sh deploy.sh mount stop update start up"
+    echo "| mount |  mount config files to /docker"
+    echo "| stop  |  stop all containers"
+    echo "| update|  update all containers"
+    echo "| start |  start all containers"
+    echo "| up    |  start all containers and exec command"
     exit 1
 }
 
-##放置挂载文件
+#放置挂载文件
 mount(){
     #挂载 nginx 配置文件
     if test ! -f "/docker/nginx/conf/nginx.conf" ;then
@@ -44,11 +45,45 @@ mount(){
     fi
 }
 
+#停止容器
+stop(){
+    docker-compose stop
+}
+
+#启动容器
+start(){
+    docker-compose up -d
+}
+
+#更新镜像
+update(){
+    docker-compose stop
+    docker-compose pull
+    docker-compose up -d
+}
+
+#运行容器
+up(){
+    docker-compose up -d
+}
+
 
 #根据输入参数，选择执行对应方法，不输入则执行使用说明
 case "$1" in
 "mount")
     mount
+;;
+"stop")
+    stop
+;;
+"update")
+    update
+;;
+"start")
+    start
+;;
+"up")
+    up
 ;;
 *)
     usage
